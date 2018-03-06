a89fe731a8cd3fb3c570e15f7e8173237ef05c37
suleka96 <sulekahelmini96comp@gmail.com>
2018-03-06 20:15:08 +0530

    declared variable app

--- server.js
+++ server.js
@@ -3,9 +3,18 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+const app =express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'ejs');
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+app.get('/', (req, res) => res.render('pages/index'));
+
+
+
+
