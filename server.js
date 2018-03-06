fdde012cff5e8db70603e557b67e482f557c477c
Raneesh Gomez <raneesh2307@gmail.com>
2018-03-06 20:51:52 +0530

    added post request handler for register json information

--- server.js
+++ server.js
@@ -4,10 +4,22 @@ const PORT = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`)); 
+app.use(express.static(path.join(__dirname, 'public'))); //Define where to look for static assets
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs'); //Define view engine as EJS
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`)); 
+
+// create application/json parser
+var jsonParser = bodyParser.json();
+
+//Index route
+app.get('/', (req, res) => res.render('pages/index'));
+
+app.post('/register', jsonParser, function(req, res) {
+    if (!req.body) return res.sendStatus(400);
+    var registerInfo = req.body;
+    console.log(registerInfo);
+});
+
