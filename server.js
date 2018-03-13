517161793415e3219d704788904c5e1c89f1d804
Raneesh Gomez <raneesh2307@gmail.com>
2018-03-13 16:35:11 +0530

    updated server.js with post handler

--- server.js
+++ server.js
@@ -8,30 +8,37 @@ var cors = require('cors');
 const app = express();
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public'))); //Define where to look for static assets
+app.use(express.static(path.join(__dirname, 'public'))); //Define path for static assets
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs'); //Define view engine as EJS
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`)); 
 
-// create application/json parser
+//Create application/json parser
 var jsonParser = bodyParser.json();
 
-//happy
-
-
-// create application/x-www-form-urlencoded parser
+//Create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-
-//Index route
+//GET request handler for index route
 app.get('/', (req, res) => res.render('pages/index'));
 
-console.log("hii");
-
+//POST request handler for register route
 app.post('/register', jsonParser, function(req, res) {
     console.log("inside register route");
     if (!req.body) return res.sendStatus(400);
     var registerInfo = req.body;
+    res.sendStatus(200).send(req.body);
     console.log(registerInfo);    
 });
+
+//POST request handler for login button
+app.post('/login', jsonParser, function(req, res) {
+    console.log("inside login route");
+    if (!req.body) return res.sendStatus(400);
+    var loginInfo = req.body;
+    res.sendStatus(200).send(req.body);
+    console.log(loginInfo);    
+});
+
+
 
